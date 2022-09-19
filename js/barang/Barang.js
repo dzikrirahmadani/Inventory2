35ab28ad2f33123293d617905b05f1d987c7fe5d
unknown <dzikrirahmadani211@gmail.com>
2022-09-19 11:08:02 +0700

    linked

--- js/barang/Barang.js
+++ js/barang/Barang.js
@@ -58,7 +58,6 @@ $(document).ready(function(){
     
     function showTable(response, i){
         return `<tr>
-                    <td>${i+1}</td>
                     <td>${response[i].kd_brg}</td>
                     <td>${response[i].nm_brg}</td>
                     <td>${response[i].nm_kategori}</td>
@@ -66,19 +65,26 @@ $(document).ready(function(){
                     <td>${response[i].stok}</td>
                     <td>${response[i].nm_merk}</td>
                     <td>${response[i].spesifikasi}</td>
-                    <td>
-<<<<<<< HEAD
-                        <button class='btn-edit px-4 p-2 bg-green-600 text-white rounded-lg' id='${response[i].id_brg}'>Ubah</button>
-                        <button class='btn-hapus px-4 p-2 bg-orange-600 text-white rounded-lg' id='${response[i].id_brg}'>Hapus</button>
-=======
-                    <button class='btn-edit px-4 p-2 bg-green-600 text-white rounded-lg' data-id="${response[i].id_brg}" id="btn-ubah">Ubah</button>
-                    <button class='btn-hapus px-4 p-2 bg-orange-600 text-white rounded-lg' data-id="${response[i].id_brg}" id='btn-hapus'>Hapus</button>
->>>>>>> 8a58afbd3ae41c1a1f4e3ce6c8a091b8c5ed02f9
+                    <td class="flex">
+                        <button class='btn-edit px-2 p-1 bg-green-600 text-white rounded-[3px]' data-id="${response[i].id_brg} id="btn-ubah">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                        </svg>
+                        </button>
+                        <button class='ml-1 btn-hapus px-2 p-1 bg-orange-600 text-white rounded-[3px]' data-id="${response[i].id_brg}" id='btn-hapus'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                            </svg>
+                        </button>
                     </td>
-                   </tr>`;
+                </tr>`;
+
+                
     }
+    
     readData();
 })
-
+        
 
 
