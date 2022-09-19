999ba2fc7ab87f99d7c8d8810884eb62a2c61282
galihroswandi <111326692+galihroswandi@users.noreply.github.com>
2022-09-19 11:42:22 +0700

    add

--- js/barang/Barang.js
+++ js/barang/Barang.js
@@ -66,7 +66,6 @@ $(document).ready(function(){
                     <td>${response[i].stok}</td>
                     <td>${response[i].nm_merk}</td>
                     <td>${response[i].spesifikasi}</td>
-<<<<<<< HEAD
                     <td class="flex">
                         <button class='btn-edit px-2 p-1 bg-green-600 text-white rounded-[3px]' data-id="${response[i].id_brg} id="btn-ubah">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
@@ -79,11 +78,6 @@ $(document).ready(function(){
                                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                             </svg>
                         </button>
-=======
-                    <td>
-                        <button class='btn-edit px-4 p-2 bg-green-600 text-white rounded-lg' data-id="${response[i].id_brg}" id="btn-ubah">Ubah</button>
-                        <button class='btn-hapus px-4 p-2 bg-orange-600 text-white rounded-lg' data-id="${response[i].id_brg}" id='btn-hapus'>Hapus</button>
->>>>>>> 00799b5f2a1655da9cca2cc07a249ac13914da46
                     </td>
                 </tr>`;
 
