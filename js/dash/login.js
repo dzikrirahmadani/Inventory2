f605a06d00861e38b24d1a8368db860b44f9f7f6
galihroswandi <111326692+galihroswandi@users.noreply.github.com>
2022-09-21 14:31:59 +0700

    perbaikan login remember password menggunakan javascript

--- js/dash/login.js
+++ js/dash/login.js
@@ -1,6 +1,36 @@
+// Cek Session login
 const session = sessionStorage.getItem('id');
 if( session ){
     document.location.href = 'dash/dashbor.html';
+}
+
+// Cek Cookie
+const getCookie = document.cookie
+  .split(';')
+  .map(cookie => cookie.split('='))
+  .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
+
+if( !getCookie ){
+    document.location.href = 'login.html';
+}else{
+    const id = getCookie.id;
+    const id_admin = getCookie.I;
+    console.log(id, id_admin)
+
+    $.ajax({
+        type : "POST",
+        url : "../php/login/CekCookie.php",
+        data : `username=${id}&id_admin=${id_admin}`,
+        dataType : "JSON",
+        success :  (response) => {
+            if( response.status == '1' ){
+                sessionStorage.setItem("id", response.hash);
+                sessionStorage.setItem("I", response.id_admin);
+
+                document.location.href = 'dash/dashbor.html';
+            }
+        }
+    })
 }
 
 $(document).ready(function(){
@@ -9,31 +39,19 @@ $(document).ready(function(){
         checkData();
     })
 
-    // const getCookie = (cname) => {
-    //     let name = cname + "=";
-    //     let ca = document.cookie.split(';');
-    //     for( let i = 0; i < ca.length; i++ ){
-    //         let c = ca[i];
-    //         while(c.charAt(0)== ''){
-    //             c = c.substring(1);
-    //         }
-
-    //         if( c.indexOf(name) == 0 ){
-    //             return c.substring(name.length, c.length);
-    //         }
-    //     }
-
-    //     return "";
-    // }
-
-    // if( getCookie('id') ){
-    //     document.location.href = 'dash/dashbor.html';
-    // }
-
     const checkData = () => {
 
         const username = $('#username').val();
         const password = $('#password').val();
+
+        // Chexbox remember
+        let chexbox = document.getElementsByName('remember');
+        let remember = '';
+        for( let i = 0; i < chexbox.length; i++ ){
+            if( chexbox[i].checked ){
+                remember += chexbox[i].value;
+            }
+        }
 
         $.ajax({
             type : "POST",
@@ -42,15 +60,30 @@ $(document).ready(function(){
             dataType : "JSON",
             success : (response) => {
                 if( response.status == 1 ){
+                    console.log(response.id_admin)
+                    if( remember ){
+                        sessionStorage.setItem("id", response.hash);
+                        sessionStorage.setItem("I", response.id_admin);
+                        setCookie('id', response.hash, 30);
+                        setCookie('I', response.id_admin, 30);
+                    }else{
+                        sessionStorage.setItem("id", response.hash);
+                        sessionStorage.setItem("I", response.id_admin);
+                    }    
                     document.location.href = 'dash/dashbor.html';
-                    sessionStorage.setItem("id", response.hash);
-                    sessionStorage.setItem("I", response.id_admin);
                 }else{
                     document.getElementById('message').innerHTML = `<i>${response.msg}</i>`;
                 }
             }
         })
 
+    }
+
+    const setCookie = (name, value, exdays) => {
+        let d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = `expires=${d.toGMTString()}`;
+        document.cookie = `${name}=${value};${expires}`;
     }
 
 })
