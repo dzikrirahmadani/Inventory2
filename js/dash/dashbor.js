c93faec66b26db8bcdae8843a6305282b1060cb3
galihroswandi <111326692+galihroswandi@users.noreply.github.com>
2022-09-24 09:44:30 +0700

    tambah animasi saat web di load halaman dashboar

--- js/dash/dashbor.js
+++ js/dash/dashbor.js
@@ -11,44 +11,54 @@ btn_out.addEventListener('click', () => {
         logout();
     }
 })
-
-function delete_cookie( name, path, domain ) {
-    if( get_cookie( name ) ) {
-      document.cookie = name + "=" +
-        ((path) ? ";path="+path:"")+
-        ((domain)?";domain="+domain:"") +
-        ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
-    }
-  }
-  
 const logout = () => {
     sessionStorage.removeItem('id');
     sessionStorage.removeItem('I');
 
-    delete_cookie('id', "/Inventory2/views", "localhost");
+    document.location.href = '../login.html';
 }
 
 const landingOn = () => {
     const landing = document.getElementById('landing');
-    // const loading = document.createElement('div');
+    const cards = document.querySelectorAll('.cards');
+    const heading = document.getElementById('heading');
 
-    // loading.textContent = "Loading";
-    // loading.style.fontSize = "1.5em";
-    // loading.style.color = "#fff";
-    // landing.appendChild(loading);
-
-    // let loaded = setInterval(() => {
-    //     loading.textContent = loading.textContent + '.';
-    // }, 200)
-
+    // Event Blank
     landing.style.opacity = '0';
     landing.style.transition = 'all 1s ease-in-out';
 
-    // setTimeout(() => {
-    //     clearInterval(loaded);
-    //     loading.style.display = 'none';
-    // }, 1000);
     setTimeout(() => {
         landing.style.display = 'none';
     }, 2000)
+    // End of envent blank
+
+    // Event cards
+    for( let i = 0; i < cards.length; i++ ){
+        setTimeout(() => {
+            cards[i].classList.add('muncul');
+            cards[i].style.transition = 'all .5s .8s ease-in-out';
+        }, 200 * (i + 1));
+    }
+    // End of event cards
+
+    // Event heading
+    heading.style.transition = 'all 1s 1s cubic-bezier(.39,.58,.58,1.26)';
+    heading.style.marginTop = '0';
+    heading.style.opacity = '1';
+    // End of event heading
 }
+
+const cards = document.querySelectorAll('.cards');
+cards.forEach(el => {
+    el.addEventListener('mouseover', function(){
+        this.style.transition = 'all .5s ease-in-out';
+        this.style.transform = "scale(1.06, 1.06)";
+        // this.style.transform = "translate(0, -10px)";
+        this.style.boxShadow = "0, 5px .1px ##1b1b29";
+    });
+    el.addEventListener('mouseleave', function(){
+        this.style.transition = "all .5s ease-in-out";
+        this.style.transform = "scale(1,1)";
+        this.style.boxShadow = "none";
+    })
+})
