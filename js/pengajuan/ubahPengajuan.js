c4eaff0fbcd64de32fcc2f9413c903d65c3124a8
galihroswandi <111326692+galihroswandi@users.noreply.github.com>
2022-09-27 10:54:44 +0700

    add popup confirm halaman pengajuan

--- js/pengajuan/ubahPengajuan.js
+++ js/pengajuan/ubahPengajuan.js
@@ -111,6 +111,15 @@ $(document).ready(function(){
         const tanggal = $('#tgl_pengajuan').val();
         const status = $('#status').val();
 
+        const message = document.getElementById('notif');
+        const pesan = document.querySelector('.pesan');
+        const text = document.querySelector('.message');
+
+        // DOM ERROR Notification
+        const logo = document.querySelector('.pesan > .logo');
+        const textErr = document.querySelector('.pesan > .message');
+        const btn_close = document.querySelector('.pesan > .close');
+
         $.ajax({
             type : "POST",
             url : "../../php/pengajuan/UbahPengajuan.php",
@@ -118,11 +127,35 @@ $(document).ready(function(){
             dataType : "JSON",
             success : (response) => {
                 if( response.status = '1' ){
-                    alert(response.msg);
+                    message.style.transition = 'all 5s 5s ease-in-out';
+                    message.style.opacity = '1';
+                    message.style.display = 'flex';
+                    pesan.style.top = '10%';
+                    text.innerHTML = `<h1 class='capitalize'>${response.msg}</h1>`;
+                    btn_close.style.display = 'none';
                     resetForm();
                     setTimeout(() => {
                         document.location.href = 'pengajuan.html';
-                    }, 2000)
+                        message.style.display = 'none';
+                        message.style.opacity = '0';
+                            pesan.style.top = '-100rem';
+                    }, 2000);
+                }else{
+                    message.style.transition = 'all 5s 5s ease-in-out';
+                    message.style.opacity = '1';
+                    message.style.display = 'flex';
+                    pesan.style.top = '10%';
+                    logo.innerHTML = `<img src="../../assets/images/gif/error.gif" alt="">`;
+                    logo.style.padding = '1rem';
+                    logo.style.boxSizing = 'border-box';
+                    textErr.innerHTML = `<h1 class='capitalize'>${response.msg}</h1>`;
+    
+                    btn_close.addEventListener('click', () => {
+                        message.style.transition = 'all 5s ease';
+                        message.style.display = 'none';
+                        message.style.opacity = '0';
+                        pesan.style.top = '-100rem';
+                    })
                 }
             }
         })
@@ -144,4 +177,21 @@ $(document).ready(function(){
     $('#submit').click(() => {
         prosesUbahData();
     })
+})
+
+const popup = document.querySelector('.modal-popup');
+// event popup box
+document.body.addEventListener('click', (e) => {
+    if( e.target.id == 'logout' ){
+      popup.style.display = 'flex';
+    }else if(e.target.id == 'confirmErr'){
+      popup.style.display = 'none';
+    }
+})
+const confirmScs = document.querySelector('.confirm > .confirmScs');
+confirmScs.addEventListener('click', () => {
+    sessionStorage.removeItem('id');
+    sessionStorage.removeItem('I');
+
+    document.location.href = '../login.html';
 })
